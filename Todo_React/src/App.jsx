f895b236d6ea4629f77513ff301428b8350acf27
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-08 23:43:25 +0530

    Change

--- Todo_React/src/App.jsx
+++ Todo_React/src/App.jsx
@@ -1,29 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { TodoProvider } from './Contexts'
+import React from 'react'
 
-function App() {
-  const [todos , setTodos] = useState([])
-  const [addTodos, setAddTodos] = useState()
-
-
-  const addTodo = (todo) => {
-    setTodos((prev) => [{id: Date.now(),...todo},...prev])
-  }
-
-  const updateTodo = (id,todo) => {
-    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id? todo : prevTodo)))
-  }
-
-
+const App = () => {
   return (
-    <TodoProvider value={{todos, addTodo , updateTodo, deleteTodo, toggleComplete}}>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
-    </TodoProvider>
+    <div>App</div>
   )
 }
 
--- Todo_React/src/Contexts/TodoContext.js
+++ Todo_React/src/Contexts/TodoContext.js
@@ -1,21 +1,10 @@
-import React, { createContext, useContext } from 'react'
+import React from 'react'
+import { createContext } from 'react'
 
 export const TodoContext = createContext({
-    todos: [
-        {
-            id: 1,
-            todo: "Todo Message",
-            completed: false
-        }
-    ],
-    addTodo: (todo) => { },
-    updateTodo: (id, todo) => { },
-    deleteTodo: (id) => { },
-    toggleComplete: (id) => { },
+
 })
 
 export const TodoProvider = TodoContext.Provider
 
-export const useTodo = () => {
-    return useContext(TodoContext)
-}
+export const useTodo
