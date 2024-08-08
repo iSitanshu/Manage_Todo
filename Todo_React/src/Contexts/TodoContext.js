f02f5464f983b833287d4f871b27224c1217e71a
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-08 23:45:35 +0530

    Context SP

--- Todo_React/src/Contexts/TodoContext.js
+++ Todo_React/src/Contexts/TodoContext.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useContext } from 'react'
 import { createContext } from 'react'
 
 export const TodoContext = createContext({
+    todos:[
 
+    ],
+    addTodo: (todo)=>{},
+    updateTodo: ()=>{},
+    deleteTodo: ()=>{},
+    toggleComplete: (id)=>{},
 })
 
 export const TodoProvider = TodoContext.Provider
 
-export const useTodo
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
