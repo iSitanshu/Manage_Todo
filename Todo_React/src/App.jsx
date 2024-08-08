86ba0b0faab07a6403726cff7405f3e6dae9b344
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-08 18:19:58 +0530

    BoilerPlate

--- Todo_React/src/App.jsx
+++ Todo_React/src/App.jsx
@@ -2,16 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { TodoProvider } from './Contexts'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos , setTodos] = useState([])
+  const [addTodos, setAddTodos] = useState()
+
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(),...todo},...prev])
+  }
+
+  const updateTodo = (id,todo) => {
+    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === todo.id? todo : prevTodo)))
+  }
+
 
   return (
-    <>
+    <TodoProvider value={{todos, addTodo , updateTodo, deleteTodo, toggleComplete}}>
       <h1 className="text-3xl font-bold underline">
       Hello world!
       </h1>
-    </>
+    </TodoProvider>
   )
 }
 
