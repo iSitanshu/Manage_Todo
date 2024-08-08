11557d8d15a578a6a5156eb0fda139ea4a0a7677
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-09 04:31:26 +0530

    Update

--- Todo_React/src/App.jsx
+++ Todo_React/src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { TodoProvider } from './Contexts/TodoContext'
+import { useState } from 'react'
 
 const App = () => {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(),...todo},...prev])
+  }
+  const updateTodo = () => {}
+  const deleteTodo = () => {}
+  const toggleComplete = () => {}
+
   return (
-    <div>App</div>
+    <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+      <h1 className='text-3xl font-bold underline'>Hellow</h1>
+    </TodoProvider>
   )
 }
 
