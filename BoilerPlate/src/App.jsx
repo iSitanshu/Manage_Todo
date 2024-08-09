5e303fefd38079b0362bbe7241080da17253e681
iSitanshu <Sitanshumishra18@gmail.com>
2024-08-09 11:12:17 +0530

    App update

--- BoilerPlate/src/App.jsx
+++ BoilerPlate/src/App.jsx
@@ -7,14 +7,15 @@ import TodoItem from './components/TodoItem'
 function App() {
   const [todos, setTodos] = useState([])
 
+  console.log(todos)
+
   const addTodo = (todo) => {
-    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev] )
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
   }
 
   const updateTodo = (id, todo) => {
-    setTodos((prev) => prev.map((prevTodo) => (prevTodo.id === id ? todo : prevTodo )))
-
-    
+    setTodos((prev) => prev.map((prevTodo) => 
+    (prevTodo.id === id ? todo : prevTodo )))
   }
 
   const deleteTodo = (id) => {
@@ -22,7 +23,6 @@ function App() {
   }
 
   const toggleComplete = (id) => {
-    //console.log(id);
     setTodos((prev) => 
     prev.map((prevTodo) => 
       prevTodo.id === id ? { ...prevTodo, 
